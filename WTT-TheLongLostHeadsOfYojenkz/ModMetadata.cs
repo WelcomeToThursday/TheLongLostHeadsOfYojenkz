461f4ebff82c61777d7f06aa4781b9584715ada7
bushtail <evannosich@icloud.com>
2026-08-05 18:07:46 -0400

    WHEN YOU CAN'T EVEN SAY - MY NAME

---
+++ WTT-TheLongLostHeadsOfYojenkz/ModMetadata.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using Range = SemanticVersioning.Range;
+using Version = SemanticVersioning.Version;
+
+namespace WTTTheLongLostHeadsOfYojenkz;
+
+[UsedImplicitly]
+public class ModMetadata : IModMetadata
+{
+    public string ModGuid { get; init; } = "com.wtt.yojenkzheads";
+    public string Name { get; init; } = "WTT-TheLongLostHeadsOfYojenkz";
+    public string Author { get; init; } = "GrooveypenguinX";
+    public List<string>? Contributors { get; init; } = ["The WTT Team"];
+    public Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version!.ToString(3));
+    public Range SptVersion { get; init; } = new("~4.1.0");
+    public bool HasPrepatcher { get; init; } = false;
+    public List<string>? Incompatibilities { get; init; }
+    public Dictionary<string, Range>? ModDependencies { get; init; } = new()
+    {
+        { "com.wtt.commonlib", new Range("~3.0.0") }
+    };
+    public string? Url { get; init; }
+    public string License { get; init; } = "MIT";
+}
--- WTT-TheLongLostHeadsOfYojenkz/WTTTheLongLostHeadsOfYojenkz.cs
+++ WTT-TheLongLostHeadsOfYojenkz/WTTTheLongLostHeadsOfYojenkz.cs
@@ -1,57 +1,36 @@
 ﻿using System.Reflection;
+using JetBrains.Annotations;
 using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
-using SPTarkov.Server.Core.Models.Spt.Mod;
-using WTTServerCommonLib.Models;
-using Range = SemanticVersioning.Range;
 
 namespace WTTTheLongLostHeadsOfYojenkz;
 
-public record ModMetadata : AbstractModMetadata
+[Injectable(TypePriority = OnLoadOrder.Preload + 2), UsedImplicitly]
+public class YojenkzHeads(WTTServerCommonLib.WTTServerCommonLib wttCommon) : IOnLoad
 {
-    public override string ModGuid { get; init; } = "com.wtt.yojenkzheads";
-    public override string Name { get; init; } = "WTT-TheLongLostHeadsOfYojenkz";
-    public override string Author { get; init; } = "GrooveypenguinX";
-    public override List<string>? Contributors { get; init; } = null;
-    public override SemanticVersioning.Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version?.ToString(3));
-    public override Range SptVersion { get; init; } = new("~4.0.1");
-    public override List<string>? Incompatibilities { get; init; }
-    public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
+    private static readonly Dictionary<string, string> CharacterAudioMap = new()
     {
-        { "com.wtt.commonlib", new Range("~2.0.6") }
+        { "Big Boss", "big_boss" },
+        { "Kaz Miller", "kaz_miller" },
+        { "Revolver Ocelot", "revolver_ocelot" },
+        { "Homelander", "homelander" },
+        { "Chris Redfield", "chris_redfield" },
+        { "Dante", "dante" },
+        { "Duke Nukem", "duke_nukem" },
+        { "Geralt", "geralt"},
+        { "Norman Reedus", "norman_reedus" },
+        { "Sam Fisher", "sam_fisher" }
     };
-    public override string? Url { get; init; }
-    public override bool? IsBundleMod { get; init; } = true;
-    public override string License { get; init; } = "MIT";
-}
 
-[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 2)]
-public class YojenkzHeads(
-    WTTServerCommonLib.WTTServerCommonLib wttCommon
-) : IOnLoad
-{
-    internal static readonly Dictionary<string, string> CharacterAudioMap = new()
-    {
-        {"Big Boss", "big_boss"},
-        {"Kaz Miller", "kaz_miller"},
-        {"Revolver Ocelot", "revolver_ocelot"},
-        {"Homelander", "homelander"},
-        {"Chris Redfield", "chris_redfield"},
-        {"Dante", "dante"},
-        {"Duke Nukem", "duke_nukem"},
-        {"Geralt", "geralt"},
-        {"Norman Reedus", "norman_reedus"},
-        {"Sam Fisher", "sam_fisher"}
-    };
-    
-    internal static readonly List<string> AudioBundleKeys = new()
+    private static readonly List<string> AudioBundleKeys = new()
     {
         "audio/yojenkz.bundle"
     };
-    public async Task OnLoad()
+    
+    public async Task OnLoadAsync(CancellationToken cancellationToken)
     {
         
-        Assembly assembly = Assembly.GetExecutingAssembly();
+        var assembly = Assembly.GetExecutingAssembly();
         await wttCommon.CustomHeadService.CreateCustomHeads(assembly);
         await wttCommon.CustomVoiceService.CreateCustomVoices(assembly);
         wttCommon.CustomAudioService.RegisterAudioBundles(AudioBundleKeys);
